7a64394796c8c836e74a71d36a106008b57681e4
SouLeha <strong0904@mail.ru>
2025-02-21 23:46:31 +0300

    исправляет вложенность модуля main-js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Импорт данных из файла утил
-import'./util.js';
+import { generateData } from './util.js';
 import { renderThumbnails } from './thumbnails.js';
 
 // Генерация данных
-/* const photosData = generateData();
-photosData(); */
+const photosData = generateData();
+photosData();
 renderThumbnails();
