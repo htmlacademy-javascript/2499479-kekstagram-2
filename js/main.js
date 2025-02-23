d6d1320db6f4188184ddadebe75f6f53ee13190e
SouLeha <strong0904@mail.ru>
2025-02-23 22:56:32 +0300

    убираю вывод в переменную фото генерэйт дата

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 //Импорт данных из файла утил
-import { generateData } from './util.js';
 import { renderThumbnails } from './thumbnails.js';
 
 // Генерация данных
-const photosData = generateData();
-
-photosData();
-
 renderThumbnails();
